d6dc18d5a7036f5fdb126532df60aa20fc1cb18d
Noor Fatima <noorfatima@Noors-MacBook-Air.local>
2025-04-01 13:19:10 +0500

    deleting custom

--- src/App.jsx
+++ src/App.jsx
@@ -63,7 +63,6 @@ function SelectPercentage({ children, percentage, onSelect }) {
         <option value="5">Average - 5%</option>
         <option value="10">Good - 10%</option>
         <option value="20">Excellent - 20%</option>
-        <option value="custom">Custom tip</option>
       </select>
     </div>
   );
